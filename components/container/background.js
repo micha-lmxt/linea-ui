e6dbefde8973d982c8b9914d931aebc6b6dd46bb
micha-lmxt <github@lucht-familie.de>
2021-04-19 21:41:15 +0200

    f

--- components/container/background.js
+++ components/container/background.js
@@ -1,8 +1,9 @@
 import {baseAction} from '../../utils';
 import { getBgColor } from '../../utils/getBgColor';
-
-export const background = baseAction((node, props) => {
+import {c} from 'linea-ui-project-css/css/c' 
+export const backgroundA = baseAction((node, props) => {
     return {classes:[
         ...getBgColor(props.color||"white",props.colorDark||"black")
     ]}
 })
+export const background = (props,...other) => c(...getBgColor(props.color||"white",props.colorDark||"black"),...other)

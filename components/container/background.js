7465a3295253080df34d82a5a3794e51554194ad
Michael Lucht <github@lucht-familie.de>
2021-04-26 16:19:32 +0200

    Update background.js

--- components/container/background.js
+++ components/container/background.js
@@ -6,6 +6,6 @@ export const backgroundA = baseAction((node, props) => {
         classes: backgroundR(props)
     }
 })
-export const backgroundR = (props, ...other) = getBgColor(props.color||"white", props.colorDark||"black").concat(other);
+export const backgroundR = (props, ...other) => getBgColor(props.color||"white", props.colorDark||"black").concat(other);
 
 export const background = (props,...other) => backgroundR(props, ...other).join(' ');

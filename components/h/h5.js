34e078adb40614dfaad21fcb3a49e588117ac7dc
Michael Lucht <github@lucht-familie.de>
2021-04-27 11:55:29 +0200

    Update h5.js

--- components/h/h5.js
+++ components/h/h5.js
@@ -1,5 +1,5 @@
 import {baseAction} from '../../utils';
-import {text_base} from 'linea-ui-project-css/css/text-base';
+import {text_xl} from 'linea-ui-project-css/css/text-xl';
 import {getColor} from '../../utils/getColor';
 import { p_2 } from 'linea-ui-project-css/css/p-2';
 import { m_2 } from 'linea-ui-project-css/css/m-2';
@@ -11,7 +11,7 @@ export const H5A = baseAction((node, props) => {
 
 export const H5R = (props={},...other) => [
         ...getColor(props.color,props.colorDark),
-        text_base,
+        text_xl,
         p_2, m_2, font_bold
     ].concat(other)
 

5de5885710b22dcab52916a2fb73f5a52d44ee42
Michael Lucht <github@lucht-familie.de>
2021-04-27 11:50:44 +0200

    Update code.js

--- components/text/code.js
+++ components/text/code.js
@@ -2,7 +2,7 @@ import {baseAction} from '../../utils';
 import {text_base} from 'linea-ui-project-css/css/text-base';
 import {getColor} from '../../utils/getColor';
 import {getBgColor} from '../../utils/getBgColor';
-import { p_2 } from 'linea-ui-project-css/css/p-2';
+import { p_4 } from 'linea-ui-project-css/css/p-4';
 import { m_2 } from 'linea-ui-project-css/css/m-2';
 import { rounded } from 'linea-ui-project-css/css/rounded';
 
@@ -15,7 +15,7 @@ export const codeR = (props={}, ...other) => [
         ...getBgColor(props.background||"black",props.backgroundDark||"white"),
         rounded,
         text_base,
-        p_2, m_2
+        p_4, m_2
     ].concat(other)
     
 export const code = (props, ...other) => codeR(props, ...other).join(' ');

624344aea4867fabb6850ad1c8b4949242e94cd4
Michael Lucht <github@lucht-familie.de>
2021-04-27 10:24:17 +0200

    Update getBgColor.js

--- utils/getBgColor.js
+++ utils/getBgColor.js
@@ -1,10 +1,14 @@
 import { bg_white } from 'linea-ui-project-css/css/bg-white';
 import { bg_black } from 'linea-ui-project-css/css/bg-black';
+import { bg_gray_light } from 'linea-ui-project-css/css/bg-gray_light';
+import { bg_gray_dark } from 'linea-ui-project-css/css/bg-gray_dark';
 import { bg_primary } from 'linea-ui-project-css/css/bg-primary';
 import { bg_secondary } from 'linea-ui-project-css/css/bg-secondary';
 import { bg_tertiary } from 'linea-ui-project-css/css/bg-tertiary';
 import { dark_bg_white } from 'linea-ui-project-css/css/dark:bg-white';
 import { dark_bg_black } from 'linea-ui-project-css/css/dark:bg-black';
+import { dark_bg_gray_light } from 'linea-ui-project-css/css/dark:bg-gray_light';
+import { dark_bg_gray_dark } from 'linea-ui-project-css/css/dark:bg-gray_dark';
 import { dark_bg_primary } from 'linea-ui-project-css/css/dark:bg-primary';
 import { dark_bg_secondary } from 'linea-ui-project-css/css/dark:bg-secondary';
 import { dark_bg_tertiary } from 'linea-ui-project-css/css/dark:bg-tertiary';
@@ -25,6 +29,12 @@ const getBg = (col) => {
             return bg_secondary_light;
         case "black":
             return bg_black;
+        case "grey-light":
+        case "gray-light":
+            return bg_gray_light;
+        case "gray-dark":
+        case "grey-dark":
+            return bg_gray_dark;
         default:
             return bg_white;
     }
@@ -41,6 +51,12 @@ const getBgDark = (col) => {
             return dark_bg_secondary_light;
         case "white":
             return dark_bg_white;
+        case "grey-light":
+        case "gray-light":
+            return dark_bg_gray_light;
+        case "gray-dark":
+        case "grey-dark":
+            return dark_bg_gray_dark;
         default:
             return dark_bg_black;
     }

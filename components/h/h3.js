bb76b434ab4ba15067883e6361495b2ba30fe633
Michael Lucht <github@lucht-familie.de>
2021-04-27 11:54:43 +0200

    Update h3.js

--- components/h/h3.js
+++ components/h/h3.js
@@ -1,5 +1,5 @@
 import {baseAction} from '../../utils';
-import {text_xl} from 'linea-ui-project-css/css/text-xl';
+import {text_3xl} from 'linea-ui-project-css/css/text-3xl';
 import {getColor} from '../../utils/getColor';
 import { p_2 } from 'linea-ui-project-css/css/p-2';
 import { m_2 } from 'linea-ui-project-css/css/m-2';
@@ -11,7 +11,7 @@ export const H3A = baseAction((node, props) => {
 
 export const H3R = (props={}, ...other) => [
         ...getColor(props.color,props.colorDark),
-        text_xl,
+        text_3xl,
         p_2,m_2, font_bold
     ].concat(other);
     

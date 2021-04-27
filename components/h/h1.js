2dae8d71b336030f096489c40659087b3588dbb4
Michael Lucht <github@lucht-familie.de>
2021-04-27 11:53:59 +0200

    Update h1.js

--- components/h/h1.js
+++ components/h/h1.js
@@ -1,8 +1,9 @@
 import {baseAction} from '../../utils';
-import {text_3xl} from 'linea-ui-project-css/css/text-3xl';
+import {text_5xl} from 'linea-ui-project-css/css/text-5xl';
 import {getColor} from '../../utils/getColor';
 import { p_2 } from 'linea-ui-project-css/css/p-2';
-import { m_2 } from 'linea-ui-project-css/css/m-2';
+import { mx_2 } from 'linea-ui-project-css/css/mx-2';
+import { my_8 } from 'linea-ui-project-css/css/my-8';
 import {font_bold} from 'linea-ui-project-css/css/font-bold'
 
 export const H1A = baseAction((node, props) => {
@@ -11,8 +12,8 @@ export const H1A = baseAction((node, props) => {
 
 export const H1R = (props={}, ...other) => [
         ...getColor(props.color,props.colorDark),
-        text_3xl,
-        p_2,m_2, font_bold
+        text_5xl,
+        p_2,mx_2,my_8, font_bold
     ].concat(other)
 
 export const H1 = (props, ...other) => H1R(props, ...other).join(' ');

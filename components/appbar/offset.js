851832ba6131c800889b073a49e1215538ad2603
Michael Lucht <github@lucht-familie.de>
2021-04-28 13:16:11 +0200

    Update offset.js

--- components/appbar/offset.js
+++ components/appbar/offset.js
@@ -1,12 +1,12 @@
 import { baseAction } from '../../utils';
-import { mt_16 } from 'linea-ui-project-css/css/mt-16';
+import { mt_20 } from 'linea-ui-project-css/css/mt-20';
 
 export const offsetA = baseAction((node, props) => {
     return {
         classes: offsetR(props)
     }
 })
-export const offsetR = (props={}, ...other) => [mt_16]
+export const offsetR = (props={}, ...other) => [mt_20]
 
 export const offset = (props,...other) => offsetR(props, ...other).join(' ');
 

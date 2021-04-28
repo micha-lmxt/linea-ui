6c8d7201984eca12911ba38d9fd64d5f801cfb19
Michael Lucht <github@lucht-familie.de>
2021-04-28 14:32:41 +0200

    Update content.js

--- components/drawer/content.js
+++ components/drawer/content.js
@@ -2,7 +2,6 @@ import { baseAction } from '../../utils';
 import { h_auto } from 'linea-ui-project-css/css/h-auto';
 import {overflow_hidden} from 'linea-ui-project-css/css/overflow-hidden';
 import { overflow_y_auto } from 'linea-ui-project-css/css/overflow-y-auto';
-import { scrolling_touch } from 'linea-ui-project-css/css/scrolling-touch';
 import { sticky } from 'linea-ui-project-css/css/sticky';
 import { top_0} from 'linea-ui-project-css/css/top-0';
 
@@ -16,7 +15,6 @@ export const contentR = (props={}, ...other) => [
   h_auto,
   overflow_hidden,
   overflow_y_auto,
-  scrolling_touch,
   sticky,
   top_0
 ].concat(other);

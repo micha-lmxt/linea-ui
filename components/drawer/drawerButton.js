338ef3082d49342c569c55c4c2b805353dac6aec
Michael Lucht <github@lucht-familie.de>
2021-04-28 16:17:09 +0200

    Update drawerButton.js

--- components/drawer/drawerButton.js
+++ components/drawer/drawerButton.js
@@ -1,10 +1,7 @@
 import { baseAction } from '../../utils';
-import { sm_opacity_0 } from 'linea-ui-project-css/css/sm:opacity-0';
-import { sm_w_0 } from 'linea-ui-project-css/css/sm:w-0';
-import { md_opacity_0 } from 'linea-ui-project-css/css/md:opacity-0';
-import { md_w_0 } from 'linea-ui-project-css/css/md:w-0';
-import { lg_opacity_0 } from 'linea-ui-project-css/css/lg:opacity-0';
-import { lg_w_0 } from 'linea-ui-project-css/css/lg:w-0';
+import { sm_hidden } from 'linea-ui-project-css/css/sm:hidden';
+import { md_hidden } from 'linea-ui-project-css/css/md:hidden';
+import { lg_hidden } from 'linea-ui-project-css/css/lg:hidden';
 import {cleanup, setClasses} from '../../utils/setClasses.js';
 import {getContext, unsubscribe} from '../../Context/keyContext';
 
@@ -33,10 +30,10 @@ export const drawerButtonA = (node, props={}) => {
 
 export const drawerButtonR = (props, ...other) => {
     const {closeBreakpoint} = props;
-    return (closeBreakpoint === "sm" ? [sm_opacity_0, sm_w_0] :
-        closeBreakpoint === "md" ? [md_opacity_0, md_w_0] :
+    return (closeBreakpoint === "sm" ? [sm_hidden] :
+        closeBreakpoint === "md" ? [md_hidden] :
             closeBreakpoint === "lg" ?
-            [lg_opacity_0, lg_w_0] : []).concat(other);
+            [lg_hidden] : []).concat(other);
 }
 
 export const drawerButton = (props, ...other) => drawerButtonR(props, ...other).join(' ')

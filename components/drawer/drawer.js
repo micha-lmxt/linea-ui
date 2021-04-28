11d10230763cbba0208f1f72c92fb9ce9303d10c
Michael Lucht <github@lucht-familie.de>
2021-04-28 14:50:21 +0200

    Update drawer.js

--- components/drawer/drawer.js
+++ components/drawer/drawer.js
@@ -4,8 +4,7 @@ import { transition_all } from 'linea-ui-project-css/css/transition-all'
 import { transform } from 'linea-ui-project-css/css/transform';
 import { fixed } from "linea-ui-project-css/css/fixed.js";
 import { top_0 } from "linea-ui-project-css/css/top-0.js";
-import { overflow_hidden } from "linea-ui-project-css/css/overflow-hidden.js";
-import { h_full } from "linea-ui-project-css/css/h-full.js";
+import { h_auto } from "linea-ui-project-css/css/h-auto.js";
 import { duration_700 } from 'linea-ui-project-css/css/duration-700';
 import { ease_out } from 'linea-ui-project-css/css/ease-out';
 import { _translate_x_full } from 'linea-ui-project-css/css/-translate-x-full';
@@ -75,8 +74,8 @@ export const drawerR = (props, ...other) => {
     return [
         ...slide, 
         right ? right_0 : left_0, 
-        fixed, top_0, overflow_hidden, 
-        h_full, z_20,
+        fixed, top_0, 
+        h_auto, z_20,
         ...bp,
         drawerSize(size),
         flex_shrink_0,

4e0599dfdaf79622e830b7776cac35815710757c
Michael Lucht <github@lucht-familie.de>
2021-04-28 13:02:04 +0200

    Update h5.js

--- components/h/h5.js
+++ components/h/h5.js
@@ -3,7 +3,7 @@ import {text_xl} from 'linea-ui-project-css/css/text-xl';
 import {getColor} from '../../utils/getColor';
 import { p_2 } from 'linea-ui-project-css/css/p-2';
 import { mx_2 } from 'linea-ui-project-css/css/mx-2';
-import { my_4} from 'linea-ui-project-css/css/my-4;
+import { my_4} from 'linea-ui-project-css/css/my-4';
 import {font_bold} from 'linea-ui-project-css/css/font-bold'
 
 export const H5A = baseAction((node, props) => {

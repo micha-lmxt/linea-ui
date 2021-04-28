e5d42efe64faea5151265b88da658461f8d9ce10
Michael Lucht <github@lucht-familie.de>
2021-04-28 11:25:12 +0200

    Update largeText.js

--- components/h/largeText.js
+++ components/h/largeText.js
@@ -2,7 +2,9 @@ import {text_3xl} from 'linea-ui-project-css/css/text-3xl';
 import {baseAction} from '../../utils';
 import {getColor} from '../../utils/getColor';
 import { p_2 } from 'linea-ui-project-css/css/p-2';
-import { m_2 } from 'linea-ui-project-css/css/m-2';
+import { mx_2 } from 'linea-ui-project-css/css/mx-2';
+import { my_4} from 'linea-ui-project-css/css/my-4;
+
 
 export const largeTextA = baseAction((node, props) => {
     return {classes:largeTextR(props)}
@@ -11,7 +13,7 @@ export const largeTextA = baseAction((node, props) => {
 export const largeTextR = (props={}, ...other) => [
         ...getColor(props.color,props.colorDark),
         text_3xl,
-        p_2, m_2
+        p_2, mx_2,my_4,
     ].concat(other)
     
 export const largeText = (props, ...other) => largeTextR(props, ...other).join(' ');
